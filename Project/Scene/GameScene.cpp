9065b93a893954f566e61cc27b14fa660332eddf
aaaaa ttttt <ryudai.0728@gmail.com>
2023-10-07 00:33:15 +0900

    enemy発生途中

--- Project/Scene/GameScene.cpp
+++ Project/Scene/GameScene.cpp
@@ -5,6 +5,7 @@ void GameScene::Initialize(GameManager* scene)
 	viewProjection.Initialize({ 0.2f,-0.6f,0.0f }, { 11.0f,5.0f,-15 });
 	player_ = make_unique<Player>();
 	player_->Initialize();
+	LoadEnemyDate();
 	scene;
 	MainCamera::Initialize();
 	//make_unique<
@@ -25,6 +26,8 @@ void GameScene::Update(GameManager* scene)
 	}
 
 	player_->Update();
+	enemy_->Update();
+	UpdateEnemyCommands();
 	
 	Collision();
 
@@ -37,6 +40,7 @@ void GameScene::Update(GameManager* scene)
 void GameScene::Draw(GameManager* scene)
 {
 	player_->Draw(viewProjection);
+	enemy_->Draw(viewProjection);
 	scene;
 }
 
@@ -51,3 +55,80 @@ void GameScene::Collision()
 	collisionManager_->CheckAllCollision();
 
 }
+
+// 敵のロードcsvで
+void GameScene::LoadEnemyDate() {
+    fileLoad = FileLoader::CSVLoadFile("enemySpawn.csv");
+}
+
+// 敵発生の更新
+void GameScene::UpdateEnemyCommands() {
+	// 待機処理
+	if (wait) {
+		waitTimer--;
+		if (waitTimer <= 0) {
+			// 待機完了
+			wait = false;
+		}
+		return;
+	}
+
+	// 1行分の文字列を入れる変数
+	std::string line;
+
+	// コマンド実行ループ
+	while (getline(fileLoad, line)) {
+		// 1行分の文字列をストリームに変換して解析しやすくする
+		std::istringstream line_stream(line);
+
+		std::string word;
+		// ,区切りで行の先頭文字列を取得
+		getline(line_stream, word, ',');
+
+		// "//"から始まる行はコメント
+		if (word.find("//") == 0) {
+			// コメント行を飛ばす
+			continue;
+		}
+
+		// POPコマンド
+		if (word.find("SPAWN") == 0) {
+			// x座標
+			getline(line_stream, word, ',');
+			float x = (float)std::atof(word.c_str());
+
+			// y座標
+			getline(line_stream, word, ',');
+			float y = (float)std::atof(word.c_str());
+
+			// z座標
+			getline(line_stream, word, ',');
+			float z = (float)std::atof(word.c_str());
+
+			EnemySpawn(Vector3(x, y, z));
+
+
+		}
+		else if (word.find("WAIT") == 0) {
+			getline(line_stream, word, ',');
+
+			// 待ち時間
+			int32_t waitTime = atoi(word.c_str());
+
+			// 待機開始
+			wait = true;
+			waitTimer = waitTime;
+
+			// コマンドループを抜ける
+			break;
+		}
+	}
+
+}
+
+// 敵の発生
+void GameScene::EnemySpawn(const Vector3& position) {
+	enemy_ = make_unique<Enemy>();
+	enemy_->Initialize(position);
+}
+
--- Project/Scene/GameScene.h
+++ Project/Scene/GameScene.h
@@ -2,8 +2,10 @@
 #include"IScene.h"
 #include"GameManager.h"
 #include"GameObject/Player/Player.h"
+#include"GameObject/Enemy/Enemy.h"
 #include"GameObject/Camera/MainCamera.h"
 #include"CollisionManager.h"
+#include"FileLoader.h"
 
 class GameScene : public IScene
 {
@@ -17,6 +19,13 @@ public:
 
 	void Draw(GameManager* scene)override;
 
+	/// <summary>
+	/// 敵発生
+	/// </summary>
+	void LoadEnemyDate();
+	void UpdateEnemyCommands();
+	void EnemySpawn(const Vector3& position);			
+	
 private:
 	
 	void Collision();
@@ -26,5 +35,11 @@ private:
 	unique_ptr<CollisionManager> collisionManager_ = nullptr;
 
 	unique_ptr<Player>player_ = nullptr;
+
+	unique_ptr<Enemy>enemy_ = nullptr; // 敵
+	bool wait = false; // 待機中フラグ
+	uint32_t waitTimer = 0; // 待機中タイマー
+	stringstream fileLoad;
 };
+
 
