73a031b9816986e2d9018cbe748f632f406e7af7
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-10-11 23:17:37 +0900

    FadeProcessクラスはメソッド化してから進める

--- Project/GameObject/FadeProcess/FadeProcess.cpp
+++ Project/GameObject/FadeProcess/FadeProcess.cpp
@@ -1,1 +1,42 @@
 #include "GameObject/FadeProcess/FadeProcess.h"
+
+
+
+// FadeProcessクラスのインスタンス取得
+FadeProcess* FadeProcess::GetInstance() {
+	static FadeProcess instance;
+	return &instance;
+}
+
+
+// 初期化処理
+void FadeProcess::Initialize() {
+
+}
+
+
+// 更新処理
+void FadeProcess::Update() {
+
+
+}
+
+
+// フェードイン処理
+void FadeProcess::Fade_In() {
+
+}
+
+
+// フェードアウト処理
+void FadeProcess::Fade_Out() {
+
+}
+
+
+// 描画処理
+void FadeProcess::Draw() {
+
+}
+
+
--- Project/GameObject/FadeProcess/FadeProcess.h
+++ Project/GameObject/FadeProcess/FadeProcess.h
@@ -3,9 +3,43 @@
 
 class FadeProcess {
 
-public:
+public: // メンバ関数
 
-private:
+	/// <summary>
+	/// FadeProcessクラスのインスタンス取得
+	/// </summary>
+	static FadeProcess* GetInstance();
+
+	/// <summary>
+	/// 初期化処理」
+	/// </summary>
+	static void Initialize();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	static void Update();
+
+	/// <summary>
+	/// フェードイン処理
+	/// </summary>
+	static void Fade_In();
+
+	/// <summary>
+	/// フェードアウト処理
+	/// </summary>
+	static void Fade_Out();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	static void Draw();
+
+private: // メンバ関数
+
+
+
+private: // メンバ変数
 
 
 };
