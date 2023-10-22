3c6799812e6481f042968608a9afc588fde33988
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-10-23 05:01:50 +0900

    とりあえず壁できた

--- Project/GameObject/ShamWall/ShamWall.cpp
+++ Project/GameObject/ShamWall/ShamWall.cpp
@@ -5,44 +5,10 @@
 // èâä˙âªèàóù
 void ShamWall::Initialize() {
 
-	// è„ï”ÇÃï«
-	for (int Index = 0; Index < MAX_WALL_OBJECT; Index++) {
-
-		// è„ï”ÇÃï«
-		/*Top_[Index].model = make_unique<Model>();
-		Top_[Index].model->CreateFromObj("ShamWall");
-		Top_[Index].model->SetColor({ 1.0f, 0.0f, 0.0f, 1.0f });
-		Top_[Index].worldTansform.scale = initScale_;
-		Top_[Index].worldTansform.translate = initTopTranslate_;
-		initTopTranslate_.z -= 2.2f;*/
-
-
-		// â∫ï”ÇÃï«
-		/*Bottom_[Index].model = make_unique<Model>();
-		Bottom_[Index].model->CreateFromObj("ShamWall");
-		Bottom_[Index].model->SetColor({ 0.0f, 1.0f, 0.0f, 1.0f });
-		Bottom_[Index].worldTansform.scale = initScale_;
-		Bottom_[Index].worldTansform.translate = initBottomTranslate_;
-		initBottomTranslate_.z -= 2.2f;*/
-
-
-		// ç∂ï”ÇÃï«
-		/*Left_[Index].model = make_unique<Model>();
-		Left_[Index].model->CreateFromObj("ShamWall");
-		Left_[Index].model->SetColor({ 0.0f, 0.0f, 1.0f, 1.0f });
-		Left_[Index].worldTansform.scale = initScale_;
-		Left_[Index].worldTansform.translate = initLeftTranslate_;
-		initLeftTranslate_.x -= 2.2f;*/
-
-
-		// âEï”ÇÃï«
-		Right_[Index].model = make_unique<Model>();
-		Right_[Index].model->CreateFromObj("ShamWall");
-		Right_[Index].model->SetColor({ 1.0f, 1.0f, 0.0f, 1.0f });
-		Right_[Index].worldTansform.scale = initScale_;
-		Right_[Index].worldTansform.translate = initRightTranslate_;
-		initRightTranslate_.x -= 2.2f;
-	}
+	shamWall_.model = make_unique<Model>();
+	shamWall_.model->CreateFromObj("ShamWall");
+	shamWall_.model->SetColor(modelColor_);
+	shamWall_.worldTansform.scale = initScale_;
 }
 
 
@@ -50,15 +16,15 @@ void ShamWall::Initialize() {
 // çXêVèàóù
 void ShamWall::Update() {
 
-	for (int Index = 0; Index < MAX_WALL_OBJECT; Index++) {
-		//Top_[Index].worldTansform.UpdateMatrix();
-		//Bottom_[Index].worldTansform.UpdateMatrix();
-		//Left_[Index].worldTansform.UpdateMatrix();
-		Right_[Index].worldTansform.UpdateMatrix();
-		Right_[Index].model->SetColor(color);
-	}
-	ImGui::Begin("color");
-	ImGui::DragFloat4("color", &color.x, 0.05f);
+	shamWall_.worldTansform.UpdateMatrix();
+	shamWall_.model->SetColor(modelColor_);
+
+	
+	ImGui::Begin("shamWall");
+	ImGui::DragFloat3("scale", &shamWall_.worldTansform.scale.x, 0.005f);
+	ImGui::DragFloat3("rotation", &shamWall_.worldTansform.rotation.x, 0.005f);
+	ImGui::DragFloat3("translate", &shamWall_.worldTansform.translate.x, 0.005f);
+	ImGui::ColorEdit4("color", &modelColor_.x);
 	ImGui::End();
 }
 
@@ -67,10 +33,6 @@ void ShamWall::Update() {
 // ï`âÊèàóù
 void ShamWall::Draw(ViewProjection view) {
 
-	for (int Index = 0; Index < MAX_WALL_OBJECT; Index++) {
-		//Top_[Index].model->Draw(Top_[Index].worldTansform, view);
-		//Bottom_[Index].model->Draw(Bottom_[Index].worldTansform, view);
-		//Left_[Index].model->Draw(Left_[Index].worldTansform, view);
-		Right_[Index].model->Draw(Right_[Index].worldTansform, view);
-	}
+	shamWall_.model->Draw(shamWall_.worldTansform, view);
 }
+
--- Project/GameObject/ShamWall/ShamWall.h
+++ Project/GameObject/ShamWall/ShamWall.h
@@ -39,20 +39,11 @@ public:
 
 private:
 
-	const static uint32_t MAX_WALL_OBJECT = 40;
-	WallElemnt Top_[MAX_WALL_OBJECT]{};
-	WallElemnt Bottom_[MAX_WALL_OBJECT]{};
-	WallElemnt Left_[MAX_WALL_OBJECT]{};
-	WallElemnt Right_[MAX_WALL_OBJECT]{};
-
-	// 初期座標
-	Vector3 initTopTranslate_ = { 33.0f, 0.0f, 40.7f };
-	Vector3 initBottomTranslate_ = { -33.0f, 0.0f, 40.7f };
-	Vector3 initLeftTranslate_ = { 40.7f, 0.0f, 33.0f };
-	Vector3 initRightTranslate_ = { 40.7f, 0.0f, -33.0f };
+	WallElemnt shamWall_;
 
 	// 初期スケール
-	Vector3 initScale_ = { 0.5f, 1.0f, 0.5f };
+	Vector3 initScale_ = { 1.07f, 2.0f, 1.07f };
 
-	Vector4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+	// カラー
+	Vector4 modelColor_ = { 1.0f, 1.0f, 1.0f, 1.0f };
 };
