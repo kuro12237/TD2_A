40a19d87f23cf0a21e82084bac90196117cb41bc
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-10-25 14:57:56 +0900

    ima

---
+++ Project/GameObject/StartCount/StartCount.cpp
@@ -0,0 +1,99 @@
+﻿#include "StartCount.h"
+
+
+
+
+void StartCount::Initialize() {
+
+	// テクスチャの読み込み
+	startCount_.TexHD = 0;
+
+
+	startCount_.worldTransform.Initialize();
+	startCount_.position = { 500.0f, 400.0f };
+
+	startCount_.sprite = make_unique<Sprite>();
+	startCount_.sprite->SetSrc(
+		startCount_.src[0].RightTop,
+		startCount_.src[0].RightBottom,
+		startCount_.src[0].LeftTop,
+		startCount_.src[0].LeftBottom);
+	startCount_.sprite->Initialize(new SpriteBoxState, startCount_.position, { 640.0f, 640.0f });
+	startCount_.sprite->SetTexHandle(startCount_.TexHD);
+	startCount_.sprite->SetColor(texColor);
+
+
+	eachCount_ = 0;
+	frame_ = 0;
+
+}
+
+
+
+void StartCount::Update() {
+
+	if (isStartCount_) {
+
+		// 制限時間の減算処理
+		SubtructTimer();
+
+		// 制限時間の加算処理
+		SubtructTimer();
+
+		// 制限時間の各位の値を求める
+		CalcTimerPlace(count_);
+
+		// 各位に合ったテクスチャを設定する
+		SetSrc();
+	}
+
+}
+
+
+
+void StartCount::Draw() {
+
+}
+
+
+
+void StartCount::SubtructTimer() {
+
+	if (isStartCount_) {
+
+		frame_++;
+
+		if (frame_ >= 60) {
+
+			count_ = count_ - 1;
+			frame_ = 0;
+		}
+
+
+		if (count_ <= 0) {
+
+			frame_ = 0;
+			count_ = 0;
+			isStartCount_ = false;
+		}
+	}
+
+}
+
+
+
+void StartCount::CalcTimerPlace(uint32_t nowTimer) {
+
+	eachCount_ = count_;
+}
+
+
+
+void StartCount::SetSrc() {
+
+	startCount_.sprite->SetSrc(
+		startCount_.src[count_].RightTop,
+		startCount_.src[count_].RightBottom,
+		startCount_.src[count_].LeftTop,
+		startCount_.src[count_].LeftBottom);
+}
--- Project/GameObject/StartCount/StartCount.h
+++ Project/GameObject/StartCount/StartCount.h
@@ -1,14 +1,68 @@
-#pragma once
+﻿#pragma once
 #include "WorldTransform.h"
 #include "Graphics/TextureManager/TextureManager.h"
+#include <Sprite/Sprite.h>
+
+
+struct SrcStartCount {
+	Vector2 RightTop;
+	Vector2 RightBottom;
+	Vector2 LeftTop;
+	Vector2 LeftBottom;
+};
+
+struct StartCountElement {
+	unique_ptr<Sprite> sprite;
+	uint32_t TexHD;
+	Vector2 position;
+	WorldTransform worldTransform;
+	SrcStartCount src[4];
+};
 
 
 class StartCount {
 
 public:
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize();
 
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+	/// <summary>
+	/// タイマーを減らす
+	/// </summary>
+	void SubtructTimer();
+
+	/// <summary>
+	/// 制限時間の各位を求める
+	/// </summary>
+	void CalcTimerPlace(uint32_t nowTimer);
+
+	/// <summary>
+	/// 各位に合ったテクスチャを設定する
+	/// </summary>
+	void SetSrc();
 
 private:
 
+	StartCountElement startCount_;
 
+	Vector4 texColor = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+	// スタートカウント
+	uint32_t nowCounr_ = 0;
+	uint32_t frame_;
+	uint32_t eachCount_;
+	uint32_t count_;
+	bool isStartCount_;
 };
